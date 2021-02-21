76fe87fbe79806f8a34649893fa9eb3f696b4094
Apisit Maneerat <Mixko50@users.noreply.github.com>
2021-02-22 01:43:35 +0700

    ZZZ

---
+++ components/Layout/LoginLayout.js
@@ -0,0 +1,60 @@
+import React, { Fragment } from 'react'
+
+const LoginLayout = (props) => {
+    return (
+        <Fragment>
+            <div className="page">
+                <div className="left-side">
+                    <div className="logo">
+                        <img src="/logo.svg"></img>
+                        <h1>ShopKo</h1>
+                    </div>
+                </div>
+                <div className="right-side">
+                    {props.children}
+                </div>
+            </div>
+            <style jsx>{`
+
+            .page {
+                width: 100%;
+                height: 100vh;
+                background-color: #fafaf2;
+                display: flex;
+            }
+            
+            .left-side {
+                width: 50vw;
+                height: 100%;
+                background-image: url(../img/login/loginb.svg);
+                background-size: 700px;
+                background-repeat: no-repeat;
+                 background-position: center;
+            }
+
+            .logo {
+                display: flex;
+                align-items: center;
+                padding: 20px 50px;
+            }
+
+            .logo > img {
+                width: 70px;
+            }
+
+            .logo > h1{
+                margin-left: 30px;
+            }
+
+            .right-side {
+                width: 50vw;
+                height: 100%;
+                padding: 30px 50px;
+            }
+
+            `}</style>
+        </Fragment>
+    )
+}
+
+export default LoginLayout
---
+++ pages/login.js
@@ -0,0 +1,19 @@
+import React, { Fragment } from 'react'
+import LoginLayout from '../components/Layout/LoginLayout'
+
+
+const login = (props) => {
+    return (
+        <Fragment>
+            <LoginLayout>
+
+            </LoginLayout>
+            <style jsx>{`
+            
+            
+            `}</style>
+        </Fragment>
+    )
+}
+
+export default login
