b1f770c3076edf9de0ac96bf1be43d2e60466aab
Apisit Maneerat <Mixko50@users.noreply.github.com>
2021-04-02 03:15:52 +0700

    [Edit] button

--- styles/account/Address.js
+++ styles/account/Address.js
@@ -1,84 +1,94 @@
-import css from 'styled-jsx/css'
+import css from "styled-jsx/css";
 
 export default css`
+    .title {
+        font-size: 30px;
+        display: flex;
+        height: 60px;
+        justify-content: space-around;
+    }
+    .add-button-box {
+        display: flex;
+        width: 100%;
+        height: 80px;
+        justify-content: flex-end;
+        border-bottom: 1px black solid;
+    }
 
-            .title {
-                 font-size: 30px;
-                display: flex;
-                height: 60px;
-                justify-content: space-around;
+    .address-setting {
+        padding: 20px 0px;
+        display: grid;
+        grid-template-columns: 150px 400px;
+        grid-template-rows: repeat(3, 55px);
+        grid-column-gap: 20px;
+    }
 
-            }
-            .add-button-box {
-                display: flex;
-                width: 100%;
-                height: 80px;
-                justify-content: flex-end;
-                border-bottom : 1px black solid;
-            }
+    .setting-box {
+        display: flex;
+        border-bottom: 1px black solid;
+    }
 
-            .address-setting {
-                padding: 20px 0px;
-                display: grid ;
-                grid-template-columns: 150px 400px;
-                grid-template-rows: repeat(3,55px);
-                grid-column-gap: 20px;
-            }
+    .add-button {
+        width: 100px;
+        height: 50px;
+        background-color: #ceadfe;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: 10px;
+        box-shadow: 0px 0px 8px rgb(197, 197, 195);
+        transition: 0.3s;
+    }
 
-            .setting-box{
-                display: flex;
-                border-bottom : 1px black solid;
-            }
+    .add-button:hover {
+        background-color: #dbc8f7;
+    }
 
-            .add-button{
-                width: 100px;
-                height: 50px;
-                background-color: #ceadfe;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                border-radius: 10px;
-            }
+    .address-setting > div:nth-child(odd) {
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+    }
 
-            .address-setting > div:nth-child(odd) {
-                display: flex;
-                justify-content: flex-end;
-                align-items: center;
+    .address-setting > div:nth-child(even) {
+        display: flex;
+        align-items: center;
+    }
 
-            }
+    .address-setting > div:nth-child(4) {
+        align-items: unset;
+    }
 
-            .address-setting > div:nth-child(even){
-                display: flex;
-                align-items: center;
-            }
+    .address-detail {
+        overflow: hidden;
+        text-overflow: ellipsis;
+        display: -webkit-box;
+        -webkit-line-clamp: 3;
+        -webkit-box-orient: vertical;
+    }
 
-            .address-setting > div:nth-child(4){
-                align-items: unset;
-            }
+    .address-edit {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        color: #6930c3;
+    }
 
-            .address-detail {
-                overflow: hidden;
-                text-overflow: ellipsis;
-                display: -webkit-box;
-                -webkit-line-clamp: 3;
-                -webkit-box-orient: vertical;
-            }
+    .address-check {
+        width: 100%;
+        height: 100px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        display: none;
+    }
 
-            .address-edit{
-                width: 100%;
-                height: 100%;
-                display: flex;
-                justify-content: space-around;
-                align-items: center;
-                color: #6930c3;
-            }
-
-            .address-check {
-                width: 100%;
-                height: 100px;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                display: none;
-            }
+    @media screen and (max-width: 1024px) {
+        .setting-box {
+            flex-direction: column;
+            padding-bottom: 30px;
+        }
+    }
 `;
--- styles/account/Payment.js
+++ styles/account/Payment.js
@@ -1,48 +1,49 @@
-import css from 'styled-jsx/css'
+import css from "styled-jsx/css";
 
 export default css`
-            
-            .title {
-                 font-size: 30px;
-                display: flex;
-                height: 60px;
-                justify-content: space-around;
+    .title {
+        font-size: 30px;
+        display: flex;
+        height: 60px;
+        justify-content: space-around;
+    }
+    .add-button-box {
+        display: flex;
+        width: 100%;
+        height: 80px;
+        justify-content: flex-end;
+        border-bottom: 1px black solid;
+    }
 
-            }
-            .add-button-box {
-                display: flex;
-                width: 100%;
-                height: 80px;
-                justify-content: flex-end;
-                border-bottom : 1px black solid;
-            }
+    .add-button {
+        width: 100px;
+        height: 50px;
+        background-color: #ceadfe;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: 10px;
+        box-shadow: 0px 0px 8px rgb(197, 197, 195);
+        transition: 0.3s;
+    }
+    .add-button:hover {
+        background-color: #dbc8f7;
+    }
 
-            .add-button{
-                width: 100px;
-                height: 50px;
-                background-color: #ceadfe;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                border-radius: 10px;
-            }
+    .payment-setting-box {
+        width: 100%;
+        height: auto;
+        border-bottom: 1px black solid;
+    }
 
-            .payment-setting-box {
-                width: 100%;
-                height: auto;
-                border-bottom: 1px black solid;
-            }
+    .payment-setting {
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        padding: 20px 0;
+    }
 
-            .payment-setting {
-                display: flex;
-                justify-content: space-around;
-                align-items: center;
-                padding: 20px 0;
-
-            }
-
-            .delete-payment {
-                color: #6930c3;
-            }
-            
+    .delete-payment {
+        color: #6930c3;
+    }
 `;
