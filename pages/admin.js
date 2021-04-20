409cad01129df61b38ac4cd8131e15a29a3f9e0d
Apisit Maneerat <Mixko50@users.noreply.github.com>
2021-04-20 14:11:32 +0700

    [Add] admin page, fix index

---
+++ pages/admin.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Fragment } from "react";
+import { Sidebar } from "../components/Layout/Sidebar";
+
+const admin = () => {
+    return (
+        <Fragment>
+            <Sidebar />
+        </Fragment>
+    );
+};
+
+export default admin;
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,12 @@
-import Head from 'next/head'
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
+import Head from "next/head";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 export default function Home() {
-  const router = useRouter();
+    const router = useRouter();
 
-  useEffect(() => router.push("/home"), []);
-  return (
-    <div>
-      Is Loading....
-    </div>
-  )
+    useEffect(() => {
+        router.push("/home"), [];
+    });
+    return <div>Is Loading....</div>;
 }
