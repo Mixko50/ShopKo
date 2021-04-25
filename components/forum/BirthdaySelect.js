50bb754c285395ff647745e092ccdc6956062222
Apisit Maneerat <Mixko50@users.noreply.github.com>
2021-04-25 15:29:30 +0700

    [Edit] BirthdaySelect (connect with database)

--- components/forum/BirthdaySelect.js
+++ components/forum/BirthdaySelect.js
@@ -1,6 +1,7 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
+import axios from "../../utils/axios";
 
 const useStyles = makeStyles((theme) => ({
     container: {
@@ -16,6 +17,13 @@ const useStyles = makeStyles((theme) => ({
 
 export default function BirthdaySelect(props) {
     const classes = useStyles();
+    const [birthdate, setbirthdate] = useState(props.birthdate);
+
+    const updateBirthdate = (ev) => {
+        axios.post("/setting/profile/birthdate", {
+            birthdate: ev,
+        });
+    };
 
     return (
         <form className={classes.container} noValidate>
@@ -27,6 +35,9 @@ export default function BirthdaySelect(props) {
                 className={classes.textField}
                 InputLabelProps={{
                     shrink: true,
+                }}
+                onChange={(ev) => {
+                    updateBirthdate(ev.target.value);
                 }}
             />
         </form>
